499a076dc7fa86a95d8164a2f9f9e64cd4550474
J-Lassauw <mail@jonas-l.net>
2018-08-09 22:07:27 +0100

    Started new master plugin socket based research sync
    Signed-off-by: Jonas Lassauw <jonas.lassauw@ecs.co.uk>

--- sharedPlugins/researchSync/index.js
+++ sharedPlugins/researchSync/index.js
@@ -7,106 +7,31 @@ class ResearchSync {
         this.config = slaveConfig;
         this.messageInterface = messageInterface;
         this.functions = this.loadFunctions();
-		
+
         this.research = {};
+
+        this.socket.on("researchDatabase", async researchDB => {
+            this.research = researchDB
+        };
 
         setInterval(() => {
             this.pollResearch();
-			setTimeout(this.doSync.bind(this), 2000);
         }, extras.researchSyncPollInterval || 5000);
-    }
 
-    pollResearch() {
-        // this.messageInterface("Polling Research\n")
-        this.messageInterface(this.functions.dumpResearch);
-    }
-
-    doSync() {
-        needle.get(this.config.masterIP + ':' + this.config.masterPort + '/api/slaves', (err, resp, slaveData) => {
-            if (err){
-				this.messageInterface("Unable to post JSON master/api/slaves, master might be unreachable");
-				return false;
-			}
-            if (resp.statusCode !== 200){
-                this.messageInterface("got error when calling slaves", resp.statusCode, resp.body);
-                return;
-            }
-
-            let needResearch = {};
-
-            Object.keys(slaveData).forEach(instanceKey => {
-                if (slaveData[instanceKey].unique === this.config.unique.toString()) {
-                    return;
-                }
-                if (!slaveData[instanceKey].hasOwnProperty('meta') || !slaveData[instanceKey].meta.hasOwnProperty('research')) {
-                    return;
-                }
-                let researchList = slaveData[instanceKey].meta.research;
-				if(researchList){
-                    Object.keys(researchList).forEach(researchName => {
-                        if (isNaN(researchList[researchName].researched) || isNaN(researchList[researchName].level)) {
-                            return;
-                        }
-                        if (needResearch.hasOwnProperty(researchName)) {
-                            if (needResearch[researchName].researched === 0) {
-                                needResearch[researchName].researched = parseInt(researchList[researchName].researched);
-                            }
-                            if (needResearch[researchName].level < parseInt(researchList[researchName].level)) {
-                                needResearch[researchName].level = parseInt(researchList[researchName].level);
-                            }
-                        }
-                         else {
-                            needResearch[researchName] = {researched: parseInt(researchList[researchName].researched), level: parseInt(researchList[researchName].level)};
-                        }
-                    });
-				}
-			});
-
-            let difference = this.filterResearchDiff(this.research, needResearch);
-
-            Object.keys(difference).forEach((key) => {
-                if (this.research[key]) {
-                    let command = this.functions.enableResearch;
-                    while(command.includes("{tech_name}")){
-                        command = command.replace("{tech_name}", key);
-                        command = command.replace("{tech_researched}", difference[key].researched);
-                        command = command.replace("{tech_level}", difference[key].level);
-                    }
-                    this.messageInterface(command);
-                    console.log('Unlocking '+ key + ': ' + (difference[key].researched === 0 ? 'false' : 'true') + ' and level ' + difference[key].level + ', was '+ (this.research[key].researched === 0 ? 'false' : 'true') + ' at level ' + this.research[key].level);
-                    this.messageInterface("Unlocking research: " + key + " at research state = " + (difference[key].researched === 0 ? 'false' : 'true') + ' and level ' + difference[key].level);
-                    this.research[key] = difference[key];
-                }
-            });
-
-            needle.post(this.config.masterIP + ':' + this.config.masterPort + '/api/editSlaveMeta', {
-                instanceID: this.config.unique,
-                password: this.config.clientPassword,
-                meta: {research: this.research}
-            }, {headers: {'x-access-token': this.config.masterAuthToken}}, function (err, resp) {
-                // success?
-            });
-        });
-    }
-
-    filterResearchDiff(localResearch, remoteResearch) {
-        let diff = {};
-        Object.keys(localResearch).forEach((key) => {
-            if (remoteResearch.hasOwnProperty(key)) {
-                if ((localResearch[key].researched === 0 && localResearch[key].researched !== remoteResearch[key].researched) || localResearch[key].level < remoteResearch[key].level) {
-                    if (!isNaN(remoteResearch[key].researched) && !isNaN(remoteResearch[key].level)) {
-                        diff[key] = {researched: remoteResearch[key].researched, level: remoteResearch[key].level};
-                    }
-                }
+        fs.watch('somedir', function (event, filename) {
+            console.log('event is: ' + event);
+            if (filename) {
+                console.log('filename provided: ' + filename);
+            } else {
+                console.log('filename not provided');
             }
         });
-        return diff;
     }
 
     loadFunctions() {
         return {
             dumpResearch: this.loadFunc("dumpResearch.lua"),
-			enableResearch: this.loadFunc("enableResearch.lua"),
+            enableResearch: this.loadFunc("enableResearch.lua"),
         };
     }
 
---
+++ sharedPlugins/researchSync/masterPlugin.js
@@ -0,0 +1,119 @@
+const fs = require("fs");
+
+class ResearchSync {
+    constructor({socket, instanceID, master}){
+        this.socket = socket;
+        this.instanceID = instanceID;
+        this.master = master;
+
+        this.lastSeen = Date.now();
+
+        (async () => {
+            setInterval(async () => {
+                this.socket.emit("researchDatabase", await this.master.getResearch());
+            }, 10000);
+            this.socket.on("getResearch", async () => {
+                this.lastSeen = Date.now();
+                this.socket.emit("researchDatabase", await this.master.getResearch());
+            });
+            this.socket.on("research_added", async data => {
+                this.lastSeen = Date.now();
+                await this.addResearch(data);
+                console.log("research_added: "+data.name);
+            });
+            this.socket.on("research_removed", async data => {
+                this.lastSeen = Date.now();
+                await this.removeResearch(data);
+                console.log("research_removed: "+data.name);
+            });
+        })();
+    }
+    async addResearch({level, name, acquired}){
+        let research = await this.master.getResearch();
+        if(!research[this.instanceID]) research[this.instanceID] = {};
+        if(!research[this.instanceID][name]) research[this.instanceID][name] = {name, level:0, acquired};
+        research[this.instanceID][name].name = name;
+        research[this.instanceID][name].level = level;
+        research[this.instanceID][name].acquired = level;
+
+        await this.master.saveResearch();
+        return true;
+    }
+    async removeResearch({name}){
+        let research = await this.master.getResearch();
+        if(!research[this.instanceID][name]){
+            return true;
+        } else {
+            research[this.instanceID][name].level = 0;
+            research[this.instanceID][name].acquired = Date.now();
+            await this.master.saveResearch();
+            return true;
+        }
+    }
+}
+
+class masterPlugin {
+    constructor({config, pluginConfig, path, socketio, express}){
+        this.config = config;
+        this.pluginConfig = pluginConfig;
+        this.pluginPath = path;
+        this.io = socketio;
+        this.express = express;
+
+        this.clients = {};
+        this.io.on("connection", socket => {
+            for(let id in this.clients){
+                if(Date.now() - this.clients[id].lastSeen > 60000){
+                    delete this.clients[id];
+                }
+            }
+            socket.on("registerResearch", data => {
+                console.log("Registered research sync "+data.instanceID);
+                this.clients[data.instanceID] = new ResearchSync({
+                    master:this,
+                    instanceID: data.instanceID,
+                    socket,
+                });
+            });
+        });
+
+        this.express.get("/api/research/getResearch", async (req,res) => {
+            res.send(await this.getResearch());
+        });
+    }
+    getResearch(){
+        return new Promise((resolve) => {
+            if(this.researchDatabase){
+                console.log(this.researchDatabase);
+                resolve(this.researchDatabase);
+            } else {
+                fs.readFile("database/researchDatabase.json", (err, data) => {
+                    if(err){
+                        this.researchDatabase = {};
+                        resolve(this.researchDatabase);
+                    } else {
+                        this.researchDatabase = JSON.parse(data);
+                        resolve(this.researchDatabase);
+                    }
+                });
+            }
+        });
+    }
+    saveResearch(){
+        return new Promise((resolve, reject) => {
+            if(this.researchDatabase){
+                console.log(this.researchDatabase);
+                fs.writeFile("database/researchDatabase.json", JSON.stringify(this.researchDatabase, null, 4), (err) => {
+                    if(err){
+                        reject(err);
+                    } else {
+                        resolve("Database successfully saved");
+                    }
+                });
+            } else {
+                resolve("Nothing to save");
+            }
+        });
+    }
+}
+module.exports = masterPlugin;
